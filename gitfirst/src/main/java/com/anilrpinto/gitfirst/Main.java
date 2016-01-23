7607b6ee1b717f8b7b5445a5e598f22deb9c7acd
Anil Pinto <anilrpinto@outlook.com>
2016-01-22 22:34:18 -0800

    Added Main to codebase.

---
+++ gitfirst/src/main/java/com/anilrpinto/gitfirst/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Jan 22, 2016
+ *
+ * Copyright 2009 Lobo Technologies, Inc. All rights reserved.
+ * Code is PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Code should not be duplicated outside the Lobo Technologies, Inc. domain.
+ */
+package com.anilrpinto.gitfirst;
+
+/**
+ *
+ *
+ * @author Anil Pinto
+ * @version 4.7, Jan 22, 2016
+ * @since 4.7
+ */
+public class Main {
+
+}
